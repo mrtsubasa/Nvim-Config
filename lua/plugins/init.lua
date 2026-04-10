d8f85e4c668831dfdbd4ce69996eb480cbbaf9a1
Tsubasa <mr.tsubasa@vk.com>
2026-04-10 04:07:22 +0200

    Add tui, noice, cord, mason, neo-tree plugins
    Register several new plugins in lua/plugins/init.lua: jrop/tuis.nvim with a <leader>m keymap to open the tui chooser; folke/noice.nvim (lazy-loaded on VeryLazy) with placeholder opts and dependencies on MunifTanjim/nui.nvim and rcarriga/nvim-notify; vyfor/cord.nvim; williamboman/mason.nvim; and nvim-neo-tree/neo-tree.nvim (v3.x) with its required dependencies and lazy=false. These additions expand UI, notification, tooling and file-explorer functionality.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,48 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+  'jrop/tuis.nvim',
+  config = function()
+    -- Optional: set up keymaps
+    vim.keymap.set('n', '<leader>m', function()
+      require('tuis').choose()
+    end, { desc = 'Choose Morph UI' })
+  end
+},
+-- lazy.nvim
+{
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+},
+{
+  'vyfor/cord.nvim'
+},
+{
+    "williamboman/mason.nvim"
+},
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+}
+
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
